cb6b528037f5ec9fa63390bffbc1101688f5a701
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2021-11-14 17:47:44 +0530

    adding new books on library

--- src/components/Sdata.js
+++ src/components/Sdata.js
@@ -3,6 +3,7 @@ import cleanCode from '../images/CleanCode.jpg'
 import computerOrganization from '../images/computer_organization.jpg'
 import selfDecipline from '../images/self_decipline.jpg'
 import theProgrameticProgrammer from '../images/ThePP.jpeg'
+import cppOops from '../images/oops.jpg'
 
 const Sdata =[
     {
@@ -34,6 +35,37 @@ const Sdata =[
         text:"Straight from the programming trenchesThe Pragmatic Programmer cuts through the increasing specialization.",
         imgSrc:theProgrameticProgrammer,
         btnLink:"https://soulspot.github.io/CodewithSoul-components/Library/Books/the-programatic-programing.html"
+    },
+    {
+        title:"OBJECT ORIENTED PROGRAMING WITH C++",
+        text:"Object-oriented programming (OOP) is a computer programming model that organizes software design around data, or objects, rather than functions and logic.",
+        imgSrc:cppOops,
+        btnLink:"https://drive.google.com/file/d/1SVNNCwJnMsm5BvrIn3R_nTQ8NXt7gqW_/preview"
+    },
+    {
+        title:"PYTHON IN ONE DAY",
+        text:"Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.",
+        imgSrc:"https://www.devgap.uk/blog/wp-content/uploads/2019/01/Screenshot-2019-01-26-at-15.43.44-min-1200x673.png",
+        btnLink:"https://drive.google.com/file/d/1xSWNar0dWIfOiWy0YMWGLDtd72RKJ_oP/preview"
+
+    },
+    {
+        title:"CODING THEORY",
+        text:"It contains a detailed and rigorous introduction to the theory of block codes and moves on to more advanced topics like BCH codes, Goppa codes and Sudan's algorithm for list decoding.",
+        imgSrc:"https://www.computersciencedegreehub.com/wp-content/uploads/2016/02/what-is-coding-1024x683.jpg",
+        btnLink:"https://drive.google.com/file/d/1mA2L9XRrSu03XoVZC5ZnUvJxWLsTpaf4/preview"
+    },
+    {
+        title:"The C PROGRAMING LANGUAGE",
+        text:"By Brian W. Kernighan and Dennis M. Ritchie.  Published by Prentice-Hall in 1988",
+        imgSrc:"https://upload.wikimedia.org/wikipedia/commons/0/0e/The_C_Programming_Language%2C_First_Edition_Cover.svg",
+        btnLink:"https://drive.google.com/file/d/1xJ8tPs1seGwWvKDoiIiw7-yCIGdWLFH7/preview"
+    },
+    {
+        title:"OBJECT ORIENTED PROGRAMING",
+        text:"The book aims at providing an all-round enrichment of knowledge in the area of object-oriented programming with C++ as the implementation of language.",
+        imgSrc:"https://m.media-amazon.com/images/I/51N4A59EAiL.jpg",
+        btnLink:"https://drive.google.com/file/d/1kZNV-Huy78dsYwZ80B2iVBIS_GnPxNeB/preview"
     }
     
 ]
